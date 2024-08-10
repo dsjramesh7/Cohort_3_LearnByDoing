2221de1d76cbd0f073eb4dfce8873018df7fc0be
dsjramesh7 <rameshdussa07@gmail.com>
2024-08-10 22:16:50 +0530

    practice

--- JavaScript/week3/synchronousCode.js
+++ JavaScript/week3/synchronousCode.js
@@ -11,8 +11,8 @@ function sum(n) {
 }
 
 const ans1 = sum(100);
-console.log(ans1);
+// console.log(ans1);
 const ans2 = sum(1000);
-console.log(ans2);
+// console.log(ans2);
 const ans3 = sum(10000);
-console.log(ans3);
+// console.log(ans3);
